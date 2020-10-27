18bc605212e953ea9724abc6bcc7feb2c6a1fa09
onizuka58 <onizuka58@users.noreply.github.com>
2020-10-27 13:07:57 +0700

    Test1

--- import.php
+++ import.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 //import.php
@@ -17,4 +18,25 @@ if(!empty($_FILES['csv_file']['name']))
  echo json_encode($data);
 }
 
+=======
+<?php
+
+//import.php
+
+if(!empty($_FILES['csv_file']['name']))
+{
+ $file_data = fopen($_FILES['csv_file']['name'], 'r');
+ fgetcsv($file_data);
+ while($row = fgetcsv($file_data))
+ {
+  $data[] = array(
+   'student_id'  => $row[0],
+   'student_name'  => $row[1],
+   'student_phone'  => $row[2]
+  );
+ }
+ echo json_encode($data);
+}
+
+>>>>>>> Stashed changes
 ?>
