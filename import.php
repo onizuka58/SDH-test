f15f860807f3c3036d62b94bac62cde390062fe9
onizuka58 <69909066+onizuka58@users.noreply.github.com>
2020-10-28 09:45:53 +0700

    Update import.php

--- import.php
+++ import.php
@@ -1,11 +1,9 @@
-<<<<<<< Updated upstream
 <?php
 
 //import.php
 
-if(!empty($_FILES['csv_file']['name']))
-{
- $file_data = fopen($_FILES['csv_file']['name'], 'r');
+
+ $file_data = fopen('tbl_student.csv', 'r');
  fgetcsv($file_data);
  while($row = fgetcsv($file_data))
  {
@@ -16,4 +14,6 @@ if(!empty($_FILES['csv_file']['name']))
   );
  }
  echo json_encode($data);
-}
+
+
+?>
